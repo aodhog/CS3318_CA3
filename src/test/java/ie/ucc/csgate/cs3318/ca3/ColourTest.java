9b2dec37242628e22f7c0c0b723dda804748f8e9
Aodh <120323406@umail.ucc.ie>
2022-11-28 18:38:39 +0000

    Previous two commits created new methods getHex and getRGB, they also created two exceptions. These now need to be tested.
    Created test to check exception is thrown when float number provided is over 1 and a test to check exception is thrown when float number provided is under 0.
    
    Created a test to ensure exception is thrown when hex string provided is too long and a test to ensure exception is thrown when hex string provided is too short
    
    Created a unit test to check that exception is thrown when Hex string provided contains a Non-Hex character.
    
    All previous unit tests pass.

--- src/test/java/ie/ucc/csgate/cs3318/ca3/ColourTest.java
+++ src/test/java/ie/ucc/csgate/cs3318/ca3/ColourTest.java
@@ -17,4 +17,91 @@ class ColourTest {
         final Colour colourHex = new Colour("01CC3F");
         assertEquals("1, 204, 63", colourHex.getRGB());
     }
+
+    @Fast
+    @Test
+    void testConstructorBadFloatOver1(){
+        boolean caught = false;
+        try {
+            final Colour colourFloat = new Colour(0.1f, 0.3f, 1.1f);
+
+        } catch (Exception e){
+            caught = true;
+        }
+        assertTrue(caught);
+    }
+
+    @Fast
+    @Test
+    void testConstructorBadFloatUnder0(){
+        boolean caught = false;
+        try {
+            final Colour colourFloat = new Colour(-0.1f, 0.3f, 1.0f);
+
+        } catch (Exception e){
+            caught = true;
+        }
+        assertTrue(caught);
+    }
+
+    @Fast
+    @Test
+    void testConstructorBadHexDigit(){
+        boolean caught = false;
+        try {
+            final Colour colourHex = new Colour("12345Z");
+
+        } catch (Exception e){
+            caught = true;
+        }
+        assertTrue(caught);
+    }
+
+    @Fast
+    @Test
+    void testConstructorBadHexLengthLong(){
+        boolean caught = false;
+        try {
+            final Colour colourFloat = new Colour("1234567");
+
+        } catch (Exception e){
+            caught = true;
+        }
+        assertTrue(caught);
+    }
+
+    @Fast
+    @Test
+    void testConstructorBadHexLengthShort(){
+        boolean caught = false;
+        try {
+            final Colour colourFloat = new Colour("12345");
+
+        } catch (Exception e){
+            caught = true;
+        }
+        assertTrue(caught);
+    }
+
+    @Fast
+    @Test
+    void getHex() throws Exception {
+        final Colour colourFloat = new Colour(0.1f, 0.3f, 1.0f);
+        final Colour colourHex = new Colour("01CC3F");
+
+        assertEquals("#194CFF", colourFloat.getHex());
+        assertEquals("#01CC3F", colourHex.getHex());
+
+    }
+
+    @Fast
+    @Test
+    void getRGB() throws Exception {
+        final Colour colourFloat = new Colour(0.1f, 0.3f, 1.0f);
+        final Colour colourHex = new Colour("01CC3F");
+
+        assertEquals("25, 76, 255", colourFloat.getRGB() );
+        assertEquals("1, 204, 63", colourHex.getRGB());
+
+    }
 }
