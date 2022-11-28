95229dd0552ee352296e2329854e626940e217e8
Aodh <120323406@umail.ucc.ie>
2022-11-28 17:52:55 +0000

    Created a colour class and a corresponding ColourTest class in the two appropriate main and test directories. Created the annotation Fast to measure runtime for each test to ensure they are quick.

---
+++ src/main/java/ie/ucc/csgate/cs3318/ca3/Colour.java
@@ -0,0 +1,4 @@
+package ie.ucc.csgate.cs3318.ca3;
+
+public class Colour {
+}
---
+++ src/test/java/ie/ucc/csgate/cs3318/ca3/ColourTest.java
@@ -0,0 +1,7 @@
+package ie.ucc.csgate.cs3318.ca3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColourTest {
+
+}
